778e30ba896bfb2e7233f129434a145b9121bdd8
Jack <1162817095@qq.com>
2021-12-03 22:05:55 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-undef */
-import WorkerTransponder from './libs/worker'
+// import WorkerTransponder from './libs/worker'
 import StorageTransponder from './libs/localStorage'
 
 const Transponder = () => {
   let Transponder
   if (typeof localStorage === 'object') {
     Transponder = StorageTransponder
-  } else if (typeof SharedWorker === 'function') {
-    // worker has some problem in SPA TODO
-    Transponder = WorkerTransponder
   }
+  // else if (typeof SharedWorker === 'function') {
+  //   // worker has some problem in SPA TODO
+  //   Transponder = WorkerTransponder
+  // }
   if (!Transponder) {
     throw new Error('this tool does not support this environment!')
   }
